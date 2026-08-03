8c9251f8ac945669f7d8d59b64d9ebeaae339054
Megala M <megalamaruthamuthuofficial@gmail.com>
2026-08-03 13:36:34 +0530

    added frontend files

---
+++ loader.js
@@ -0,0 +1,15 @@
+// ===========================
+// Hide Loader
+// ===========================
+
+window.addEventListener("load", function () {
+
+    const loader = document.getElementById("loader");
+
+    if (loader) {
+
+        loader.style.display = "none";
+
+    }
+
+});
